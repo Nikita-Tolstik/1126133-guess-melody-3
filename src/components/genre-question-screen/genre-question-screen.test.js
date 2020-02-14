9f2a552a3c139797fc1c6d76e95001b155aa7bbe
Nikita <tolstik-nik@mail.ru>
2020-02-14 20:29:50 +0300

    Написал тест для жанра и создал snapshot

---
+++ src/components/genre-question-screen/genre-question-screen.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import GenrequestionScreen from './genre-question-screen.jsx';
+import {GameType} from '../../const.js';
+
+const question = {
+  type: GameType.GENRE,
+  genre: `Rock`,
+  answers: [
+    {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    },
+    {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `blues`,
+    },
+    {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `jazz`,
+    },
+    {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }
+  ]
+};
+
+it(`Render <GenrequestionScreen />`, () => {
+
+  const tree = renderer.create((
+    <GenrequestionScreen
+
+      question={question}
+      onAnswer={() => {}}
+    />
+  ), {
+    createNodeMock: () => {
+      return {};
+    }
+  }
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+
+});
